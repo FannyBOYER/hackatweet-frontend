768d6c388026c26a8c32b8709e5666bd011d87e2
Your name <ludmilarose1019@gmail.com>
2025-07-17 16:04:24 +0200

    page acceuil + sign up design

--- components/SignIn.js
+++ components/SignIn.js
@@ -2,13 +2,14 @@ import styles from '../styles/SignIn.module.css';
 
 function SignIn () {
     return(
-        <body className={styles.body}>
-            <div>
+            <div className={styles.container}>              
             <input type="text" placeholder="Username" id="signIpUsername"/>
             <input type="password" placeholder="Password" id="signIpPassword"/>
             <button className={styles.btn1}>SignIp</button> 
-		    </div>
-        </body >
+            <img src="Logo Twitter.png" alt="logo" className={styles.logo}/>
+            <h2 className={styles.h2}>Connect to Hackatweet</h2>
+        <button className={styles.btn2} onClick={() => handleSigninClick()}>Signin</button>
+      </div>
     )
 }
 
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,27 +1,33 @@
-  import styles from '../styles/SignUp.module.css';
+import styles from '../styles/SignUp.module.css';
 
-  function SignUp () {
-  return(
-        <header className={styles.header}>
+function SignUp() {
+  return (
+            <div className={styles.container}>
             <div className={styles.Left}>
-            <img src="logotweeter.png" alt="logo" className={styles.logoLeft}/> 
+            <img src="Logo Twitter.png" className={styles.logoLeft}/> 
             <img src="loginImage.png" alt="image" className={styles.imageLeft}/>
             </div> 
 
+           <div className={styles.containerRight}> 
             <div>
             <img src="Logo Twitter.png" className={styles.logo} />
             </div>
+
+            <div>
             <h1 className = {styles.h1}> See What's happening</h1>
             <h3 className= {styles.h3}>Join Hackatweet today.</h3>
 			<div> 
             <input type="text" placeholder='FirstName'id="signUpFirstName"/>
             <input type="text" placeholder="Username" id="signUpUsername"/>
             <input type="password" placeholder="Password" id="signUpPassword"/>
-            <button className={styles.btn1}>SignUp</button> 
-            <button className={styles.btn2}>SignIn</button>
-		    </div>
-        </header >
-    )
-}
+              <button className={styles.btn1} onClick={() => handleSignUp()}>Signup</button>
+              <p className={styles.p}>Already have an account?</p>
+              <button className={styles.btn2} onClick={() => handleSignIn()}>Signin</button>
+            </div>
+        </div>
+     </div>
+</div>
+  )
+};
 
 export default SignUp;
