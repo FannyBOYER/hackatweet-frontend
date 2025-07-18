b48fd8fa70e65fb90eb3b7efd679469bbc9d924a
Dal ferro Christian <christiandalferro@gmail.com>
2025-07-18 11:31:32 +0200

    encore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Login from '../components/Login';
+import Home from '../components/Home';
+
 
 function Index() {
-  return <Login />;
+ // return <Login />;
+ return <Home />
 }
 
 export default Index;
